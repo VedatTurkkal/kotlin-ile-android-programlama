f8ff21df3281eeb27c5c57f3fc6f3e6a3c8a30b9
VedatTurkkal <0ToRust@archood.localdomain>
2025-01-13 23:18:26 +0300

    Values cannot change after assigned

--- temel-veri-tipleri/src/main/kotlin/Main.kt
+++ temel-veri-tipleri/src/main/kotlin/Main.kt
@@ -2,5 +2,11 @@ fun main() {
     // Variable - var
     var name: String = "Ahmet"
     name = "Toprak"
-    print(name)
+    println(name)
+
+    // Value - val
+    val pi: Double = 3.14
+    var yaricap: Int = 2
+    var cevre: Double = 2 * pi * yaricap
+    println(cevre)
 }
